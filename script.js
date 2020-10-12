83d9aa9465c20988dee8216554d7b44d52ecb234
Aneta Anet <kralik.anet@seznam.cz>
2020-10-12 17:05:05 +0000

    J

--- script.js
+++ script.js
@@ -6,11 +6,34 @@ function jednicka() {
 
 }
 
-function vypocet() {
+function vysledek() {
   let c1 = document.getElementById("cislo1").valueAsNumber;
   let c2 = document.getElementById("cislo2").valueAsNumber;
-  let vysl = c1 + c2;
-  console.log(vysl);
+  let op = document.getElementById("operace").value;
 
+  let vysl = "?";
+  if (isNaN(c1)) {
+    alert("Neni zadano prvni cislo");
+  } 
+  
+  else if (isNaN(c2)) {
+    alert("Neni zadano druhe cislo");
+  } 
+  
+  else if (op == "plus"){
+    vysl = c1 + c2;
+  } 
+  
+  else if (op == "minus") {
+    vysl = c1 - c2;
+  }
+  else if (op == "nasobeni") {
+    vysl = c1 * c2;
+  }
+  else if (op == "deleni") {
+    vysl = c1 / c2;
+  }
+  document.getElementById("vysl").value = vysl;
+  document.getElementById("vysledek2").innerHTML = "Výsledek je <b>" + vysl + "<b>.";
   
 }
